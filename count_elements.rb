0409da95f8784830ecbcb91e934be30212d46bdb
hpetrow <heather.petrow@gmail.com>
2015-06-08 09:16:44 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
   # code goes here
+  histogram = {}
+  array.each{ |elt|
+    if (histogram.has_key?(elt))
+      histogram[elt] += 1
+    else
+      histogram[elt] = 1
+    end
+  }
+  histogram
 end
- 
